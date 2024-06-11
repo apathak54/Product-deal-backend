1f34cb60db0827f43b7acd4e5dd0c17ae723c0c4
Yuvraj Singh <yuvrajsinghwahla08cs@gmail.com>
2024-06-11 20:02:35 +0530

    Added CORS changes

--- server.js
+++ server.js
@@ -11,8 +11,10 @@ import userRoutes from './routes/userRoutes.js';
 import workspaceRoutes from './routes/workspaceRoutes.js'
 import clientRoutes from './routes/clientRoutes.js';
 
-const app = express();
+dotenv.config({path:'.env'});
 
+const app = express();
+app.use(express.json());
 app.use(cors({
   origin: '*', // Allow requests from any origin
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Allow specific HTTP methods
@@ -20,7 +22,12 @@ app.use(cors({
   credentials: true, // Allow cookies and authentication
   exposedHeaders: ['Set-Cookie'] // Allow exposing cookies
 }));
-dotenv.config({path:'.env'});
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  next();
+});
 
 
 
